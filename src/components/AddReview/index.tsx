62b727e7a1a3f597f9e7158ed0c0415fd57cfdd8
ishagoyal <iamishagoyal@gmail.com>
2023-12-09 19:29:18 +0530

    Fix text area

--- src/components/AddReview/index.tsx
+++ src/components/AddReview/index.tsx
@@ -11,24 +11,17 @@ const onFinishFailed = (errorInfo: any) => {
   console.log("Failed:", errorInfo);
 };
 
-type FieldType = {
-  username?: string;
-  password?: string;
-  remember?: string;
-};
-
 const AddReview: React.FC = () => (
   <Form
     name="basic"
     labelCol={{ span: 8 }}
     wrapperCol={{ span: 16 }}
     style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
     onFinish={onFinish}
     onFinishFailed={onFinishFailed}
     autoComplete="off"
   >
-    <Form.Item label="Comment">
+    <Form.Item name="textarea" label="Comment">
       <TextArea rows={4} />
     </Form.Item>
 
