5db9a9eb177ffda3263c14992b05483c69b66928
Aman Tayal <tayal.aman.aman@gmail.com>
2023-12-09 20:58:53 +0530

    fix fetchRating

--- src/hooks/interact/fetchCompanyRatings.ts
+++ src/hooks/interact/fetchCompanyRatings.ts
@@ -8,28 +8,37 @@ async function fetchCompanyRatings(
   signerOrProvider: Signer | Provider,
   companyDomain: string
 ): Promise<{
-  companyRating: number[][];
+  companyRating: number[];
   ratingHashes: string[];
 }> {
   console.log("calling fetch companies");
   const contract = new Contract(contractAddress, ABI.abi, signerOrProvider);
   const tx = await contract.fetchCompanyRatings(companyDomain);
   const entries = tx.toString().split(",");
-  const ratings = entries.slice(0, (entries.length * 5) / 6);
-  const hashes = entries.slice((entries.length * 5) / 6, entries.length);
 
   const commentIds: string[] = [];
-  const compRatings: number[][] = [];
+  const compRatings: number[]= [0 , 0 , 0 , 0, 0];
+ 
+  if (entries.length < 5) {
+    return {
+      companyRating: compRatings,
+      ratingHashes: commentIds,
+    };
+  }
+  const ratings = entries.slice(0, 5);
+  const hashes = entries.slice(5, entries.length);
+
+  for (let i = 0; i < 5; i++) {
+    if (ratings[i] !== '') {
+      compRatings[i] = parseInt(ratings[i]);
+    }
+  }
+
   for (let i = 0; i < hashes.length; i++) {
     if (hashes[i] === "") {
       continue;
     }
     commentIds.push(hashes[i]);
-    const rating: number[] = [];
-    for (let j = 0; j < 5; j++) {
-      rating.push(parseInt(ratings[i * 5 + j]));
-    }
-    compRatings.push(rating);
   }
   console.log(compRatings, commentIds);
   return {
