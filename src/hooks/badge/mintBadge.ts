68f7b343363008077d43a9f4fb48a5827dfd175b
Rohit Ranjan <rohitrjn629@gmail.com>
2023-12-10 06:44:43 +0530

    add nft creattion

--- src/hooks/badge/mintBadge.ts
+++ src/hooks/badge/mintBadge.ts
@@ -1,7 +1,7 @@
-import { Contract, Provider, Signer, keccak256 } from "ethers";
+import { Contract, Provider, Signer, keccak256, toUtf8Bytes } from "ethers";
 import ABI from "./nftABI.json";
 
-const nftAdress = "0x4721c2e91970c47ad85bee2fe34e34c359268329";
+const nftAdress = "0xdb7891c555a74afbbd5c8a7402eb6a613571b7f2";
 
 export default async function mintBadge(
   signerOrProvider: Signer | Provider,
@@ -10,7 +10,7 @@ export default async function mintBadge(
 ) {
   console.log("calling mintBadge", companyDomain, username);
   const contract = new Contract(nftAdress, ABI.abi, signerOrProvider);
-  const Hash = keccak256(username);
+  const Hash = keccak256(toUtf8Bytes(username));
   const tx = await contract.safeMint(companyDomain, Hash);
   console.log(tx);
 }
